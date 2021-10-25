32793e16ec3ee7848eea51c4e9e12de6aa70bf71
gooliajulia929 <juliadwyer93@gmail.com>
2021-10-25 10:49:52 -0400

    added warmup buttons and adjusted css styling

--- design-my-day/src/components/WarmUps.js
+++ design-my-day/src/components/WarmUps.js
@@ -2,6 +2,12 @@
 const WarmUps = () => {
     return (
         <div id='warmups'>
+            <button>Typing</button>
+            <button>Spanish</button>
+            <button>Coding</button>
+            <button>Reflection</button>
+            <button>News</button>
+            <button>Vocabulary</button>
 
         </div>
     )
