e3ae468a06653f1b729104d986d268f9766271d8
gooliajulia929 <juliadwyer93@gmail.com>
2021-10-25 12:33:48 -0400

    working on user create account and login forms

--- design-my-day/src/components/Homepage.js
+++ design-my-day/src/components/Homepage.js
@@ -1,27 +1,93 @@
 import GreetingHeader from './GreetingHeader.js'
 import Today from './Today.js'
-import {useEffect, useState} from 'react';
+import { useState} from 'react';
 import Habits from './Habits.js';
 import MyTasks from './MyTasks.js';
 import FollowingDays from './FollowingDays.js';
-
+import axios from 'axios';
 
 const HomePage = ({name}) => {
-    const [holdName, setHoldName] = useState('')
-    console.log(name)
+    const [usersName, setUsersName] = useState('Julia');
+    const [newUserName, setNewUserName] = useState('');
+    const [newUserPassword, setNewUserPassword] = useState('');
+    const [newUserFirstName, setNewUserFirstName] = useState('');
+    const [newUserConfirmPassword, setNewUserConfirmPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState(false);
 
-    useEffect((name) => {
-        setHoldName(name);
-    }, [])
 
+    const API_URL = 'https://api.airtable.com/v0/app8BFd6lNH00rllQ/Users?api_key=keyxASDYlQZpfwP0J'
 
+    const handleLogInSubmit = (ev) => {
+        ev.preventDefault();
+        console.log('log in attempted');
+        const getUserInfo = async () => {
+            console.log('getting user info...');
+
+            const resp = await axios.get(`${API_URL}`);
+            console.log(resp.data.records);
+        }
+        getUserInfo();
+    }
+
+    const createNewAccount = async (ev) => {
+        ev.preventDefault();
+        console.log('create account attempted');
+
+        const newUser = {
+            records: [
+                {
+                    fields: {
+                        username: newUserName,
+                        password: newUserPassword,
+                        firstName: newUserFirstName
+                    }
+                }
+            ]
+        }
+        await axios.post(`${API_URL}`, newUser);
+
+    }
 
 
     return (
         <div id='homepage' >
+            <div id='user-log-in'>
+                <h1>Hello, if you have an account, click 'Log In', otherwise, click 'Create Account'...</h1>
+                <div id='user-forms'>
+                    <form id='log-in' onSubmit={handleLogInSubmit}>
+                        <h2>Log In: </h2>
+                        <label htmlFor='username'>Username: </label>
+                        <input type='text' id='username' placeholder='username' />
+                        <br/>
+                        <br/>
+                        <label htmlFor='password'>Password: </label>
+                        <input type='password' placeholder='password' />
+                        <br/>
+                        <br/>
+                        <input type='submit' />
+                    </form>
+                    <form id='create-account' onSubmit={createNewAccount}>
+                        <h2>Create new acount: </h2>
+                        <label htmlFor='create-username'>Username: </label>
+                        <input type='text' id='create-username' placeholder='username' onChange={(ev) => setNewUserName(ev.target.value)}/>
+                        <br/>
+                        <br/>
+                        <label htmlFor='create-password'>Create password: </label>
+                        <input type='password' id='create-password' placeholder='password' onChange={(ev) => setNewUserName(ev.target.value)}/>
+                        <br/>
+                        <br/>
+                        <label htmlFor='confirm-password' >Confirm password: </label>
+                        <input type='password' id='confirm-password' placeholder='confirm password' />
+                        <br/>
+                        <br/>
+                        <input type='submit' />
+                    </form>
+                </div>
+
+            </div>
             <div id='greeting-tasks'>
             <GreetingHeader
-            name={holdName}/>
+            name={usersName}/>
             <MyTasks />
             </div>
             <div id='habits-today'>
