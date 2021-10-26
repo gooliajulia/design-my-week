22f468eb143d415ffc676e71caf1d931e57b46f5
gooliajulia929 <juliadwyer93@gmail.com>
2021-10-26 17:20:11 -0400

    Quick Add Toggle functionality

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,27 +1,42 @@
 import AddTaskForm from './AddTaskForm.js';
+import {useState} from 'react';
 
 
 const MyTasks = ({addNewTask, task, setTask, taskImp, setTaskImp, taskUrg, setTaskUrg, taskEnj, setTaskEnj, taskEstTime, setTaskEstTime}) => {
+
+    const [quickAddToggle, setQuickAddToggle] = useState(false);
+    const [quickAddButtonText, setQuickAddButtonText] = useState('Quick Add +')
+
+    const quickAdd = () => {
+        console.log('Quick add button was clicked')
+        setQuickAddToggle(!quickAddToggle);
+        quickAddToggle ?  setQuickAddButtonText('Quick Add +') : setQuickAddButtonText('Hide Quick Add') ;
+    }
+
+
     return (
         <div id='my-tasks'>
             <h1 className='my-tasks'>My Tasks</h1>
             <div id='my-task-buttons'>
-                <button>Quick Add + </button>
-                <button>Manage Tasks</button>
+                <button id='quick-add' className={ quickAddToggle ? 'show' : 'no-show'} onClick={quickAdd} >{quickAddButtonText}</button>
+                <button >Manage Tasks</button>
             </div>
-            <AddTaskForm 
-                    addNewTask={addNewTask}
-                    task={task}
-                    setTask={setTask}
-                    taskImp={taskImp}
-                    setTaskImp={setTaskImp}
-                    taskUrg={taskUrg} 
-                    setTaskUrg={setTaskUrg}
-                    taskEnj={taskEnj} 
-                    setTaskEnj={setTaskEnj}
-                    taskEstTime={taskEstTime}
-                    setTaskEstTime={setTaskEstTime}
-                />
+            { quickAddToggle ? 
+                        <AddTaskForm 
+                        addNewTask={addNewTask}
+                        task={task}
+                        setTask={setTask}
+                        taskImp={taskImp}
+                        setTaskImp={setTaskImp}
+                        taskUrg={taskUrg} 
+                        setTaskUrg={setTaskUrg}
+                        taskEnj={taskEnj} 
+                        setTaskEnj={setTaskEnj}
+                        taskEstTime={taskEstTime}
+                        setTaskEstTime={setTaskEstTime}
+                    />
+            : null}
+
         </div>
     )
 }
