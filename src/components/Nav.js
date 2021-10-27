179f1d414913244484bb82a0777eac6464ee8174
gooliajulia929 <juliadwyer93@gmail.com>
2021-10-27 18:02:23 -0400

    nav bar additions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,17 @@
+import {Link} from 'react-router-dom'
 
 const Nav = () => {
     return (
         <div id='nav-bar'>
+            <Link className='nav' to='/homepage/:usersName'>
+                <h3>My Homepage</h3>
+            </Link>
+            <Link className='nav' to='/'>
+                <h3>Manage Tasks</h3>
+            </Link>
+            <Link className='nav' to='/'>
+                <h3>Log Out</h3>
+            </Link>
 
         </div>
     )
